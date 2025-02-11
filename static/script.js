9661847e30a82ea751019dc949e018286a321122
Lovro <lovroperaic@gmail.com>
2025-02-11 18:33:28 +0100

    make time period buttons selectable

--- static/script.js
+++ static/script.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-
     
+    let timeButtons = Array.from(document.querySelectorAll("nav button"));
+    timeButtons.map(x => x.addEventListener('click', (e) => {
+        for (let button of timeButtons) {
+            button.classList.remove('selected');
+            e.target.classList.add('selected');
+        }
+    }));
 
 });
