b6dd4dfd8c84dad51e93ce812868a67074de5bc4
Lovro <lovroperaic@gmail.com>
2025-02-12 10:28:17 +0100

    set up setTimeFrame function and fix event listener for buttons

--- static/script.js
+++ static/script.js
@@ -1,11 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
     
-    let timeButtons = Array.from(document.querySelectorAll("nav button"));
+    let timeButtons = Array.from(document.querySelectorAll('nav button'));
     timeButtons.map(x => x.addEventListener('click', (e) => {
         for (let button of timeButtons) {
             button.classList.remove('selected');
-            e.target.classList.add('selected');
         }
+        e.target.classList.add('selected');
+        setTimeFrame(e.target.id);
     }));
+
+
+    let previousTimes = document.querySelectorAll('.previous')
+    function setTimeFrame(timeFrame) {
+        for (let p of previousTimes) {
+            p.classList.remove('day', 'week', 'month');
+            p.classList.add(timeFrame);
+        }
+    }
 
 });
