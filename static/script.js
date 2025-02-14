fae06faaf6000be8759c0e49500af4c6197e04e1
Lovro <lovroperaic@gmail.com>
2025-02-14 09:29:27 +0100

    Fix

--- static/script.js
+++ static/script.js
@@ -22,15 +22,8 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function updateTimes(timeFrame) {
-        fetch('/projects/frontend-mentor/time-tracking-dashboard-main/data.json').then((response) => {
+        fetch('data.json').then((response) => {
             if (!response.ok) {
-                /* For the sake of the static GH Pages site, the 'data.json' data was copied
-                   into the 'staticWebInfo' binding which is returned in case of a failed response.
-                   Otherwise, a new error would be thrown and the 'catch' method would populate
-                   the time elements with 'N/A'. */
-                return staticWebInfo;
-
-                /* The code would normally be: */
                 throw new Error(response.status);
             }
             return response.json();
@@ -53,111 +46,3 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
 });
-
-
-
-/* The data from the 'data.json' file for the sake of the static website hosted on GH Pages */
-let staticWebInfo = [
-    {
-      "title": "Work",
-      "timeframes": {
-        "daily": {
-          "current": 5,
-          "previous": 7
-        },
-        "weekly": {
-          "current": 32,
-          "previous": 36
-        },
-        "monthly": {
-          "current": 103,
-          "previous": 128
-        }
-      }
-    },
-    {
-      "title": "Play",
-      "timeframes": {
-        "daily": {
-          "current": 1,
-          "previous": 2
-        },
-        "weekly": {
-          "current": 10,
-          "previous": 8
-        },
-        "monthly": {
-          "current": 23,
-          "previous": 29
-        }
-      }
-    },
-    {
-      "title": "Study",
-      "timeframes": {
-        "daily": {
-          "current": 0,
-          "previous": 1
-        },
-        "weekly": {
-          "current": 4,
-          "previous": 7
-        },
-        "monthly": {
-          "current": 13,
-          "previous": 19
-        }
-      }
-    },
-    {
-      "title": "Exercise",
-      "timeframes": {
-        "daily": {
-          "current": 1,
-          "previous": 1
-        },
-        "weekly": {
-          "current": 4,
-          "previous": 5
-        },
-        "monthly": {
-          "current": 11,
-          "previous": 18
-        }
-      }
-    },
-    {
-      "title": "Social",
-      "timeframes": {
-        "daily": {
-          "current": 1,
-          "previous": 3
-        },
-        "weekly": {
-          "current": 5,
-          "previous": 10
-        },
-        "monthly": {
-          "current": 21,
-          "previous": 23
-        }
-      }
-    },
-    {
-      "title": "Self Care",
-      "timeframes": {
-        "daily": {
-          "current": 0,
-          "previous": 1
-        },
-        "weekly": {
-          "current": 2,
-          "previous": 2
-        },
-        "monthly": {
-          "current": 7,
-          "previous": 11
-        }
-      }
-    }
-]
